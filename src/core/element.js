11ace553843e08a3a53299051fb06f917a0f7a97
tisontang(唐霜) <tisontang@tencent.com>
2022-02-23 14:43:40 +0800

    fix: 修复直传属性不给值的情况

--- src/core/element.js
+++ src/core/element.js
@@ -28,7 +28,8 @@ class SFC_Element extends HTMLElement {
     const attrs = {};
     each(attributes, ({ name, value }) => {
       if (name in mapping) {
-        props[mapping[name]] = tryParse(value, true);
+        // 如果规定了prop，那么当直接使用该属性时，表达为true，例如 <t-button ghost> 其中 ghost 为 true
+        props[mapping[name]] = value === null ? true : tryParse(value, true);
       } else if (name[0] === ':') {
         props[camelcase(name.substr(1))] = tryParse(value);
       } else if (name.indexOf('data-') === 0) {
